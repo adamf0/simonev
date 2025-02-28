f14fd4480f558d45bf35ec82ed28d3bd2c3f97d0
!Github@251423! <studiocode52@gmail.com>
2025-02-28 14:45:37 +0700

    debug

--- app/Http/Controllers/BankSoalController.php
+++ app/Http/Controllers/BankSoalController.php
@@ -28,7 +28,7 @@ class BankSoalController extends Controller
         $data = BankSoal::find($id_bank_soal);
         $fakultas = Fakultas::select(DB::raw("kode_fakultas as id"),DB::raw("nama_fakultas as nama"))->get();
         $prodi = Prodi::select(DB::raw("kode_prodi as id"),DB::raw("nama_prodi as nama"))->get();
-        $mahasiswa = AkunSimak::select(DB::raw("userid as id"), "nama")->where("level","MAHASISWA")->get();
+        $mahasiswa = collect([]); //AkunSimak::select(DB::raw("userid as id"), "nama")->where("level","MAHASISWA")->get()
         $unit = Pengangkatan::select('unit_kerja')->distinct()->get();
 
         return Inertia::render('BankSoal/BankSoalForm', ['typeEvent' => "Edit", "dataBankSoal"=>$data, "listUnit"=>$unit, "listFakultas"=>$fakultas, "listProdi"=>$prodi, "listMahahsiswa"=>$mahasiswa, "level"=>session()->get('level')]);
