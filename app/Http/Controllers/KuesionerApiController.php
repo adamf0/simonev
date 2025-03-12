95e724b16e8a438e84782b7ceb2c55ec6c9b3012
!Github@251423! <studiocode52@gmail.com>
2025-03-12 15:27:06 +0700

    debug

--- app/Http/Controllers/KuesionerApiController.php
+++ app/Http/Controllers/KuesionerApiController.php
@@ -296,7 +296,7 @@ class KuesionerApiController extends Controller
                                     ->where('id_template_jawaban', $id_template_pilihan)
                                     ->first();
 
-                    dd($dataInput, $request->data);
+                    dd($dataInput, $request->data, $id_kuesioner, $id_template_pertanyaan, $id_template_pilihan);
                     $insertData[] = [
                         'id_kuesioner' => $id_kuesioner,
                         'id_template_pertanyaan' => $id_template_pertanyaan,
