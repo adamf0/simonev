45f08c072b87bd2c848e8b46a5a2b4be0f283bd0
!Github@251423! <studiocode52@gmail.com>
2025-03-13 09:31:56 +0700

    debug

--- app/Http/Middleware/XssSanitization.php
+++ app/Http/Middleware/XssSanitization.php
@@ -15,11 +15,11 @@ class XssSanitization
      */
     public function handle(Request $request, Closure $next): Response
     {
-        $input = $request->all();
-        array_walk_recursive($input, function(&$input) {
-            $input = strip_tags($input);
-        });
-        $request->merge($input);
+        // $input = $request->all();
+        // array_walk_recursive($input, function(&$input) {
+        //     $input = strip_tags($input);
+        // });
+        // $request->merge($input);
 
         return $next($request);
     }
