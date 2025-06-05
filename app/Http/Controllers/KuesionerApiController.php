f4a8bcc714bbcfed51b55343d3a315757cde1b51
!Github@251423! <studiocode52@gmail.com>
2025-06-05 09:43:50 +0700

    debug

--- app/Http/Controllers/KuesionerApiController.php
+++ app/Http/Controllers/KuesionerApiController.php
@@ -104,6 +104,8 @@ class KuesionerApiController extends Controller
         $results = $results->whereBetween(DB::raw('NOW()'),[DB::raw('start_repair'),DB::raw('end_repair')])
                         ->orderByDesc('kuesioner.tanggal')
                         ->get();
+                        
+        return $results->toRawSql();
 
         // $results = $results->transform(function ($item) use($request){
         //                     $yearEntry = date('Y', strtotime($item->tanggal));
