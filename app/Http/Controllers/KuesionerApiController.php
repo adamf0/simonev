2577cf31e62164a78057753c8e343343c0af95cc
adamf <adamilkom00@gmail.com>
2025-06-19 09:56:03 +0700

    debug

--- app/Http/Controllers/KuesionerApiController.php
+++ app/Http/Controllers/KuesionerApiController.php
@@ -326,7 +326,6 @@ class KuesionerApiController extends Controller
                     default=>'nip',
                 };
 
-                $bankSoal['rule']['target_list'] = array_map('strtolower', $bankSoal['rule']['target_list']);
                 dd($bankSoal['rule']['type'], $bankSoal['rule']['target_type'], $bankSoal['rule']['target_list']);
                 if(in_array($bankSoal['rule']['type'],["spesific","all"]) && $bankSoal['rule']['target_type']=="npm" && in_array($request->target,$bankSoal['rule']['target_list'])){
                     if($bankSoal['rule']['generate']['type']=="recursive"){
