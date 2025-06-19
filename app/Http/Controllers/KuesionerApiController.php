cf0cfffb263f2c8e38dd0903fe547747bb6f467d
adamf <adamilkom00@gmail.com>
2025-06-19 10:29:05 +0700

    debug

--- app/Http/Controllers/KuesionerApiController.php
+++ app/Http/Controllers/KuesionerApiController.php
@@ -327,14 +327,6 @@ class KuesionerApiController extends Controller
                 };
 
                 // dd($bankSoal['rule']['type'], $bankSoal['rule']['target_type'], array_map('strtolower',$bankSoal['rule']['target_list']));
-                dd(
-                    in_array($bankSoal['rule']['type'],["spesific","all"]),
-                    $bankSoal['rule']['target_type']=="prodi", 
-                    $bankSoal['rule']['target_list'], ["all"],
-                        in_array($bankSoal['rule']['target_list'], ["all"]), 
-                        $request->prodi,array_map('strtolower', $bankSoal['rule']['target_list']),
-                        in_array($request->prodi,array_map('strtolower', $bankSoal['rule']['target_list']))
-                );
                 if(
                     in_array($bankSoal['rule']['type'],["spesific","all"]) && 
                     $bankSoal['rule']['target_type']=="npm" && 
