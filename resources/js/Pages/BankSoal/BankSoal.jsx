93d4f54b69f878324107f9d455266eef5166547c
!Github@251423! <studiocode52@gmail.com>
2025-06-23 14:46:01 +0700

    debug

--- resources/js/Pages/BankSoal/BankSoal.jsx
+++ resources/js/Pages/BankSoal/BankSoal.jsx
@@ -463,6 +463,7 @@ BankSoal.BankSoalsRow = ({ level, item, loading, changeSelected, openEdit, chang
         return [...output];
     }
     const target_list_all = item.rule?.target_list_all ?? [];
+    const match = (target_list_all ?? []).some(item => listTarget.includes(item) || item === "all");
 
     console.log(
         item.id,
@@ -534,7 +535,7 @@ BankSoal.BankSoalsRow = ({ level, item, loading, changeSelected, openEdit, chang
                         <i className="bi bi-eye text-black" style={{ fontSize: "1.2rem" }}></i>
                     </button>
                     {
-                        (level=="fakultas" && item.createdBy=="admin" && item.branch==0 && item.rule?.type=="spesific" && ((target_list_all ?? []).includes(listTarget) || (["all"]).includes(listTarget.concat(["all"]))) && (item.rule?.target_type=="prodi" || item.rule?.target_type=="unit")) && 
+                        (level=="fakultas" && item.createdBy=="admin" && item.branch==0 && item.rule?.type=="spesific" && match && (item.rule?.target_type=="prodi" || item.rule?.target_type=="unit")) && 
                         <button className="btn" disabled={loading} onClick={() => createBranch(item.id)}>
                             <i className="bi bi-signpost-split text-black" style={{ fontSize: "1.2rem" }}></i>
                         </button>
