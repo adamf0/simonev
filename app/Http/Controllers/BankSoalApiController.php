3c9e68ebcd1d56a96cbb5f0d761120cac1468dea
!Github@251423! <studiocode52@gmail.com>
2025-06-23 13:33:22 +0700

    debug

--- app/Http/Controllers/BankSoalApiController.php
+++ app/Http/Controllers/BankSoalApiController.php
@@ -62,6 +62,10 @@ class BankSoalApiController extends Controller
                 ->pluck('text')
                 ->toArray();
 
+                if($item->id==123){
+                    dd($target_list, $rule);
+                }
+
                 $number = 3;
                 $rule["target_list"] = count($target_list)>$number? array_merge(array_slice($target_list, 0, $number), ["+".(count($target_list)-$number)." prodi"]):$target_list;
                 $rule["target_list_all"] = count($target_list)? $target_list:$rule["target_list"];
