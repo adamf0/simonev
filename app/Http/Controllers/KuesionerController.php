c7345bdaee9ad8e8bdbd035215cdb4a0828180cf
!Github@251423! <studiocode52@gmail.com>
2025-06-25 13:57:08 +0700

    debug

--- app/Http/Controllers/KuesionerController.php
+++ app/Http/Controllers/KuesionerController.php
@@ -153,7 +153,7 @@ class KuesionerController extends Controller
             );
         
 
-        $bankSoal = $results->whereBetween(DB::raw('NOW()'),[DB::raw('start_repair'),DB::raw('end_repair')])->toRawSqldebug();
+        $bankSoal = $results->whereBetween(DB::raw('NOW()'),[DB::raw('start_repair'),DB::raw('end_repair')])->toRawSql();
         dd($bankSoal);
 
         $bankSoal = $bankSoal->filter(function($items) use($peruntukan,$target){
