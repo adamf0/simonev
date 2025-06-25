c6605a386e637a943556b9004702d460344d0dc8
!Github@251423! <studiocode52@gmail.com>
2025-06-25 12:52:38 +0700

    debug

--- app/Http/Controllers/KuesionerController.php
+++ app/Http/Controllers/KuesionerController.php
@@ -127,6 +127,12 @@ class KuesionerController extends Controller
         }
         dump($bankSoal);
         $bankSoal = $filter->filter(function($items) use($now){
+            dump(
+                [
+                    strtotime($now),strtotime($items->start_repair." 00:00:00"),strtotime($items->end_repair." 23:59:59"),
+                    strtotime($now) >= strtotime($items->start_repair." 00:00:00"), strtotime($now) <= strtotime($items->end_repair." 23:59:59")
+                ]
+            );
             return strtotime($now) >= strtotime($items->start_repair." 00:00:00") && strtotime($now) <= strtotime($items->end_repair." 23:59:59");
         })->values();
         dump($bankSoal);
