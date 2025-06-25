eb3c3a11d49033afbe672077a1d19549fcfe8aad
!Github@251423! <studiocode52@gmail.com>
2025-06-25 10:49:42 +0700

    debug

--- app/Http/Controllers/KuesionerApiController.php
+++ app/Http/Controllers/KuesionerApiController.php
@@ -59,7 +59,7 @@ class KuesionerApiController extends Controller
         }
 
         if($request->filled("peruntukan") && $request->filled("data")){
-            $results = $results->whereNotNull("$kolom")->where("$kolom", $request->data);
+            $results = $results;
             
             $bank_soal = $bank_soal->selectRaw("
                 id as id_bank_soal,
