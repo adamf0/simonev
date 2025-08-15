0a07a045648fb994e4d947956233274c6704c329
!Github@251423! <studiocode52@gmail.com>
2025-08-15 10:18:25 +0700

    debug

--- resources/js/Pages/Tes.jsx
+++ resources/js/Pages/Tes.jsx
@@ -101,7 +101,7 @@ export default function Tes() {
 
   const makePieConfig = (labels, rawData) => {
     const total = rawData.reduce((sum, val) => sum + val, 0);
-    const percentageData = rawData.map(val => (val / total * 100).toFixed(1)+"%");
+    const percentageData = rawData.map(val => (val / total * 100).toFixed(1));
   
     return {
       data: {
@@ -121,17 +121,19 @@ export default function Tes() {
       options: {
         responsive: true,
         plugins: {
-            legend: {display: false},
-            tooltip: {
-                callbacks: {
-                label: function (context) {
-                    const rawValue = context.dataset.customData[context.dataIndex];
-                    const total = context.dataset.customData.reduce((sum, v) => sum + v, 0);
-                    const percentage = ((rawValue / total) * 100).toFixed(1);
-                    return `${context.label}: ${rawValue} (${percentage}%)`;
+            legend: {
+                display: false // kita matikan legend default
+              },
+                tooltip: {
+                    callbacks: {
+                    label: function (context) {
+                        const rawValue = context.dataset.customData[context.dataIndex];
+                        const total = context.dataset.customData.reduce((sum, v) => sum + v, 0);
+                        const percentage = ((rawValue / total) * 100).toFixed(1);
+                        return `${context.label}: ${rawValue} (${percentage}%)`;
+                    }
+                    }
                 }
-                }
-            }
         }
       }
     };
