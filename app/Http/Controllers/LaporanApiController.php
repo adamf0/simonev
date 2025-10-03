661b6e3bd29eb20983f7c3dfa3e65ff783db94ab
!Github@251423! <studiocode52@gmail.com>
2025-10-03 17:46:25 +0700

    debug

--- app/Http/Controllers/LaporanApiController.php
+++ app/Http/Controllers/LaporanApiController.php
@@ -512,7 +512,6 @@ class LaporanApiController extends Controller
                                             });
                                         });
                                     }
-                                    dd($results->toRawSql());
                                     $results = $results->count();
                                                 
                                     $jawaban->jawaban = $jawaban->isFreeText? "Lainnya":$jawaban->jawaban;
