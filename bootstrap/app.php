13014456cf34f97031bf8146ae8c0587925fb693
!Github@251423! <studiocode52@gmail.com>
2026-05-05 15:25:18 +0700

    update

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,6 +5,7 @@ use App\Http\Middleware\XssSanitization;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Maatwebsite\Excel\ExcelServiceProvider;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -13,6 +14,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
         api: __DIR__ . '/../routes/api.php',
     )
+    ->withProviders([
+        ExcelServiceProvider::class,
+    ])
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->validateCsrfTokens(except: [
             'login',
